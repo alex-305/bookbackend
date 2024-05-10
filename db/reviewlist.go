00674a3e9cedfca1f1e3b76458dcafe7d91881af
Alex Gonzalez <galex2003@icloud.com>
2024-05-10 00:10:44 -0400

    Got user review list to work

--- db/reviewlist.go
+++ db/reviewlist.go
@@ -2,16 +2,19 @@ package db
 
 import (
 	"database/sql"
+	"fmt"
+	"log"
 
 	"github.com/alex-305/bookbackend/models"
 )
 
 func (db DB) GetUserReviewList(username string, o models.SortOptions) ([]models.Review, error) {
-	query := `SELECT * FROM reviews WHERE username = $1 ORDER BY $2 $3 LIMIT $4 OFFSET $5;`
+	query := fmt.Sprintf(`SELECT * FROM reviews WHERE username = $1 ORDER BY %s %s LIMIT $2 OFFSET $3;`, o.By, o.Direction)
 
-	rows, err := db.Query(query, username, o.By, o.Direction, o.Limit, o.Page*o.Limit)
+	rows, err := db.Query(query, username, o.Limit, o.Page*o.Limit)
 
 	if err != nil {
+		log.Printf("%s", err)
 		return []models.Review{}, err
 	}
 	return db.GetReviewList(rows)
