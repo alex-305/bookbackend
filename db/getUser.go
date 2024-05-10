ae53ae5a7655b1bb47ea96bc66b25d7b263937e4
Alex Gonzalez <galex2003@icloud.com>
2024-05-09 20:02:23 -0400

    Get user now returns email

--- db/getUser.go
+++ db/getUser.go
@@ -5,10 +5,10 @@ import (
 )
 
 func (db *DB) GetUser(username string) (models.User, error) {
-	row := db.QueryRow("SELECT username, description, join_date FROM users WHERE username = $1", username)
+	row := db.QueryRow("SELECT username, email, description, join_date FROM users WHERE username = $1", username)
 
 	var user models.User
-	err := row.Scan(&user.Username, &user.Description, &user.Join_date)
+	err := row.Scan(&user.Username, &user.Email, &user.Description, &user.Join_date)
 
 	if err != nil {
 		return models.User{}, err
