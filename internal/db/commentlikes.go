2db85920ba3a4671fd60e0bb936a3c711b28b097
Alex Gonzalez <galex2003@icloud.com>
2024-05-20 19:54:07 -0400

    Made it so you could not infinitely unlike

--- internal/db/commentlikes.go
+++ internal/db/commentlikes.go
@@ -1,5 +1,7 @@
 package db
 
+import "errors"
+
 func (db DB) PostCommentLikes(user, commentid string) error {
 	tx, err := db.Begin()
 	if err != nil {
@@ -34,12 +36,23 @@ func (db DB) DeleteCommentLikes(user, commentid string) error {
 		return err
 	}
 
-	_, err = tx.Exec(`DELETE FROM user_likes_comment WHERE username = $1 AND commentid = $2`, user, commentid)
+	result, err := tx.Exec(`DELETE FROM user_likes_comment WHERE username = $1 AND commentid = $2`, user, commentid)
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+	rowsAffected, err := result.RowsAffected()
 
 	if err != nil {
 		tx.Rollback()
 		return err
 	}
+
+	if rowsAffected == 0 {
+		tx.Rollback()
+		return errors.New("could not delete from user_likes_comment")
+	}
+
 	tx.Commit()
 	return err
 }
--- internal/db/reviewlikes.go
+++ internal/db/reviewlikes.go
@@ -1,5 +1,7 @@
 package db
 
+import "errors"
+
 func (db DB) PostReviewLikes(user, reviewid string) error {
 	tx, err := db.Begin()
 	if err != nil {
@@ -34,12 +36,25 @@ func (db DB) DeleteReviewLikes(user, reviewid string) error {
 		return err
 	}
 
-	_, err = tx.Exec(`DELETE FROM user_likes_review WHERE username = $1 AND reviewid = $2`, user, reviewid)
+	result, err := tx.Exec(`DELETE FROM user_likes_review WHERE username = $1 AND reviewid = $2`, user, reviewid)
 
 	if err != nil {
 		tx.Rollback()
 		return err
 	}
+
+	rowsAffected, err := result.RowsAffected()
+
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	if rowsAffected == 0 {
+		tx.Rollback()
+		return errors.New("could not delete from user_likes_review")
+	}
+
 	tx.Commit()
 	return err
 }
